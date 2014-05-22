7b55872e9d8caae0913d3c4a054ee5fb1b85a8d4
Pelayo Parejo Pagador <pelayo.parejopagador@epitech.eu>
2014-05-22 21:05:20 +0000

    GameScene first version

---
+++ src/game/GameScene.cpp
@@ -0,0 +1,31 @@
+#include	"GameScene.hh"
+
+/* CONSTRUCTORS */
+
+GameScene::GameScene(SceneArgument const & args)
+  : AScene("GameScene")
+{
+
+}
+
+GameScene::~GameScene(void)
+{
+
+}
+
+/* MEMBER FUNCTION */
+
+bool    GameScene::initialize(void)
+{
+    return false;
+}
+
+bool    GameScene::update(void)
+{
+    return false;
+}
+
+bool    GameScene::draw(void)
+{
+    return false;
+}
