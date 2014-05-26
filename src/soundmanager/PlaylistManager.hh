9af42b516aea063d2135a57f808695f41da7ed71
Damien sauvalle <damien.sauvalle@epitech.eu>
2014-05-26 16:08:13 +0200

    add Playlist Manager

---
+++ src/soundmanager/PlaylistManager.hh
@@ -0,0 +1,25 @@
+#ifndef		_PLAYLIST_MANAGER_HH_
+# define	_PLAYLIST_MANAGER_HH_
+
+# include <vector>
+# include "SoundManager.hh"
+
+class		PlaylistManager
+{
+public:
+  PlaylistManager(SoundManager& son);
+  ~PlaylistManager();
+
+  void addPlaylist(const std::string&);
+  void deletePlaylist();
+  void playPlaylist();
+  bool update();
+  void next();
+
+private:
+  std::vector<std::string> _playlist;
+  SoundManager& _son;
+  unsigned int _n;
+};
+
+#endif
