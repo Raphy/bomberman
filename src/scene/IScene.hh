19e36c15a5b5ad4f4710ec555b72a8002551e9d7
Barthelemy Delemotte <barthelemy.delemotte@epitech.eu>
2014-06-02 17:26:41 +0200

    IScene: complete prototype of virtuals methods

--- src/scene/IScene.hh
+++ src/scene/IScene.hh
@@ -1,6 +1,21 @@
 #ifndef ISCENE_H_
 # define ISCENE_H_
 
+
+# include <SdlContext.hh>
+# include <glm/glm.hpp>
+# include <glm/gtc/matrix_transform.hpp>
+# include <iostream>
+# include <OpenGL.hh>
+# include <Game.hh>
+# include <Clock.hh>
+# include <Input.hh>
+# include <SdlContext.hh>
+# include <Geometry.hh>
+# include <Texture.hh>
+# include <BasicShader.hh>
+# include <Model.hh>
+
 # include "SceneId.hh"
 # include "SceneStatus.hh"
 
@@ -14,8 +29,8 @@ public:
     virtual ~IScene() {};
 
     virtual bool initialize() = 0;
-    virtual bool update() = 0;
-    virtual bool draw() = 0;
+    virtual bool update(gdl::Clock const& clock, gdl::Input& input) = 0;
+    virtual bool draw(gdl::AShader& shader, gdl::Clock const& clock) = 0;
 
     virtual SceneStatus const& getStatus() const = 0;
     virtual SceneId const& getId() const = 0;
