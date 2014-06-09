620be1220cfcc926633eae2d76c744fc17fda4f1
Barthelemy Delemotte <barthelemy.delemotte@epitech.eu>
2014-06-09 19:02:27 +0200

    MapText and MapTextLoader

---
+++ src/maptext/MapTextLoader.hh
@@ -0,0 +1,19 @@
+#ifndef MAPTEXTLOADER_HH_
+# define MAPTEXTLOADER_HH_
+
+# include "MapText.hh"
+
+class MapTextLoader : public MapText {
+
+private:
+    std::ifstream m_file;
+
+public:
+    MapTextLoader(std::string const& pathname);
+
+private:
+    void split(std::string const& line, StrVector& vector) const;
+    bool readLine(std::string& line_buf);
+};
+
+#endif // !MAPTEXTLOADER_HH_
