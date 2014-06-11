69ceab7f7a936206672efc042e0f6663f9cba61b
Damien sauvalle <damien.sauvalle@epitech.eu>
2014-06-11 15:14:54 +0200

    add Cursor for MenuScene

---
+++ src/menu/Cursor.cpp
@@ -0,0 +1,65 @@
+# include "Cursor.hh"
+
+Cursor::Cursor(const std::string& path, const glm::vec3& position, const glm::vec3& scale)
+{
+  _texturePath = path;
+  _position = position;
+  _position.x += -30;
+  _scale = scale;
+}
+
+Cursor::~Cursor()
+{
+
+}
+
+bool Cursor::initialize()
+{
+  if (_texture.load(_texturePath) == false)
+    {
+      std::cerr << "Cannot load the widget texture" << std::endl;
+      return false;
+    }
+
+  _geometry.setColor(glm::vec4(1, 1, 1, 1));
+
+  _geometry.pushVertex(glm::vec3(0, 0, 0));
+  _geometry.pushVertex(glm::vec3(1, 0, 0));
+  _geometry.pushVertex(glm::vec3(1, 1, 0));
+  _geometry.pushVertex(glm::vec3(0, 1, 0));
+
+  _geometry.pushUv(glm::vec2(0.0f, 1.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 1.0f));
+  _geometry.pushUv(glm::vec2(1.0f, 0.0f));
+  _geometry.pushUv(glm::vec2(0.0f, 0.0f));
+
+  _geometry.build();
+
+  return true;
+}
+
+void Cursor::update(gdl::Clock const& clock, gdl::Input& input)
+{
+
+}
+
+void Cursor::draw(gdl::AShader & shader, gdl::Clock const& clock)
+{
+  (void)clock;
+
+  shader.bind();
+
+  glm::mat4 transform(1);
+
+  transform = glm::translate(transform, _position);
+  transform = glm::scale(transform, _scale);
+
+  _texture.bind();
+
+  _geometry.draw(shader, transform, GL_QUADS);
+}
+
+void Cursor::setPosY(const glm::vec3& position)
+{
+  _position.y = position.y;
+}
---
+++ src/menu/Cursor.hh
@@ -0,0 +1,29 @@
+#ifndef CURSOR_HH_
+# define CURSOR_HH_
+
+#include <string>
+#include "AObject.hh"
+
+class Cursor : public AObject
+{
+public:
+  Cursor(const std::string& path, const glm::vec3& position, const glm::vec3& scale);
+  virtual ~Cursor();
+
+  virtual bool initialize();
+  virtual void update(gdl::Clock const& clock, gdl::Input& input);
+  virtual void draw(gdl::AShader& shader, gdl::Clock const& clock);
+
+  void setPosY(const glm::vec3&);
+
+protected:
+  gdl::Texture _texture;
+  gdl::Geometry _geometry;
+
+  std::string _texturePath;
+  glm::vec3 _scale;
+  glm::vec3 _position;
+
+};
+
+#endif
