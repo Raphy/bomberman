b658a30c0bbb019af460a44de29a55c8e450ab3b
Damien sauvalle <damien.sauvalle@epitech.eu>
2014-06-11 19:45:03 +0200

    Option Menu

--- src/menu/OptionMenu.cpp
+++ src/menu/OptionMenu.cpp
@@ -1,2 +1,119 @@
 #include "OptionMenu.hh"
 
+OptionMenu::OptionMenu(SceneArguments const& arg)
+  : AMenuScene("OptionMenu")
+{
+  addButton("./build/assets/img/soundfx.tga", glm::vec3(60, 50, 1), glm::vec3(100, 100, 0) , static_cast<ButtonHandler>(&OptionMenu::soundfxhandler), 0);
+  addButton("./build/assets/img/soundmusic.tga", glm::vec3(60, 180, 1), glm::vec3(100, 100, 0) , static_cast<ButtonHandler>(&OptionMenu::soundmusichandler), 1);
+  addButton("./build/assets/img/back.tga", glm::vec3(60, 450, 1), glm::vec3(100, 100, 0) , static_cast<ButtonHandler>(&OptionMenu::backhandler), 2);
+
+  _cursor = new Cursor("./build/assets/img/bombe.tga", glm::vec3(30, 100, 1), glm::vec3(50, 50, 0));
+
+}
+
+OptionMenu::~OptionMenu()
+{
+  delete _cursor;
+}
+
+bool OptionMenu::initialize()
+{
+  setTexture("./build/assets/img/menu.tga");
+  AMenuScene::initialize();
+
+  _cursor->initialize();
+
+  std::map<AWidget* , ButtonHandler>::iterator it;
+  it = _mapButton.begin();
+
+  while (it != _mapButton.end())
+    {
+      it->first->initialize();
+      it++;
+    }
+  return true;
+}
+
+bool OptionMenu::update(gdl::Clock const& clock, gdl::Input& input)
+{
+  // j'avoue c'est crade mais il est 2h30 du mat' A REGLER
+
+  if (input.getKey(SDLK_UP) && !_btnUp)
+    {
+      _cursorPos--;
+      _btnUp = true;
+      if (_cursorPos < 0)
+	_cursorPos = 2;
+    }
+  if (!input.getKey(SDLK_UP) && _btnUp)
+    _btnUp = false;
+
+  if (input.getKey(SDLK_DOWN) && !_btnDown)
+    {
+      _btnDown = true;
+      _cursorPos++;
+      if (_cursorPos == 3)
+	_cursorPos = 0;
+    }
+  if (!input.getKey(SDLK_DOWN) && _btnDown)
+    _btnDown = false;
+
+  if (input.getKey(SDLK_SPACE) && !_btnSpace)
+    {
+      std::map<AWidget* , ButtonHandler>::iterator it;
+
+      _btnSpace = true;
+
+      it = _mapButton.begin();
+      while (it != _mapButton.end())
+	{
+	  if (it->first->getCur() == _cursorPos)
+	    (this->*(it->second))();
+	  it++;
+	}
+    }
+  if (!input.getKey(SDLK_SPACE) && _btnSpace)
+    _btnSpace = false;
+  return true;
+}
+
+bool OptionMenu::draw(gdl::AShader& shader, gdl::Clock const &clock)
+{
+  AMenuScene::draw(shader, clock);
+
+  glDisable(GL_DEPTH_TEST);
+  glAlphaFunc(GL_GREATER, 0.3f);
+  glEnable(GL_ALPHA_TEST);
+
+  std::map<AWidget* , ButtonHandler>::iterator it;
+  it = _mapButton.begin();
+
+  while (it != _mapButton.end())
+    {
+      if (it->first->getCur() == _cursorPos)
+	_cursor->setPosY(it->first->getPos());
+      it->first->draw(shader, clock);
+      it++;
+    }
+
+  _cursor->draw(shader, clock);
+
+  return true;
+}
+
+void OptionMenu::soundfxhandler()
+{
+  std::cout << "sound FX handler ok\n";
+}
+
+void OptionMenu::soundmusichandler()
+{
+  std::cout << "sound MUSIC handler ok\n";
+}
+
+void OptionMenu::backhandler()
+{
+  std::cout << "back handler ok\n";
+}
+
+
--- src/menu/OptionMenu.hh
+++ src/menu/OptionMenu.hh
@@ -2,11 +2,29 @@
 #define OPTIONMENU_HH_
 
 #include "AMenuScene.hh"
+#include "Cursor.hh"
 
 class OptionMenu : public AMenuScene
 {
+public:
+  OptionMenu(SceneArguments const&);
+  ~OptionMenu();
 
+  virtual bool initialize();
+  virtual bool update(gdl::Clock const& clock, gdl::Input& input);
+  virtual bool draw(gdl::AShader & shader, gdl::Clock const &clock);
 
+  /* definir le handler de chaque boutton*/
+  void soundfxhandler();
+  void soundmusichandler();
+  void backhandler();
+
+private:
+  Cursor *_cursor;
+
+  bool _btnUp = false;
+  bool _btnDown = false;
+  bool _btnSpace = false;
 
 };
 
