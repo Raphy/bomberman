245f9bfe27022e7663db4100820f423fa9795c9a
Barthelemy Delemotte <barthelemy.delemotte@epitech.eu>
2014-06-11 22:09:30 +0200

    Sound loader + son qui segfault

---
+++ src/sound/SoundsLoader.cpp
@@ -0,0 +1,63 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+#include "SoundsLoader.hh"
+#include "SoundManager.hh"
+
+std::string const SoundsLoader::SOUNDS_DIR = "./build/sounds";
+std::string const SoundsLoader::CONF_FILE = "sounds.conf";
+
+SoundManager& SoundsLoader::s_sound_manager = SoundManager::getInstance();
+
+void SoundsLoader::operator()() {
+
+    std::cout << "sounds loader" << std::endl;
+
+    std::string filename = SOUNDS_DIR + "/" + CONF_FILE;
+    std::ifstream f(filename);
+
+    if (f) {
+        size_t line_no = 0;
+        std::string line;
+        while (getline(f, line)) {
+            line_no++;
+            std::istringstream line_stream(line);
+            std::vector<std::string> words;
+            copy(std::istream_iterator<std::string>(line_stream),
+                std::istream_iterator<std::string>(),
+                std::back_inserter(words));
+            if (words.size() != 3) {
+                std::cerr << "warning: " << filename << ":"
+                    << line_no << ": invalid syntax" << std::endl;
+            }
+            else {
+                std::string sound_type = words[0];
+                std::string sound_tag = words[1];
+                std::string sound_file = SOUNDS_DIR + "/" + words[2];
+
+                bool success = true;
+                if (sound_type == "fx") {
+                    success = s_sound_manager.loadFx(sound_tag, sound_file);
+                }
+                else if (sound_type == "music") {
+                    success = s_sound_manager.loadMusic(sound_tag, sound_file);
+                }
+                else {
+                    std::cerr << "warning: " << filename << ":"
+                        << line_no << ": unknown type '"
+                        << sound_type << "'" << std::endl;
+                }
+                if (success == false) {
+                    std::cerr << "warning: " << filename << ":"
+                        << line_no << ": could't load '"
+                        << sound_file << "'" << std::endl;
+                }
+            }
+        }
+    }
+}
