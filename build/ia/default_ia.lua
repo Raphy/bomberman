ad908a0265e4e751abf943795017edf95612e721
lefebv_z <marie@marie-HP-EliteBook-8560p.(none)>
2014-06-13 19:20:24 +0200

    IA] deplacement de fichiers suite...

--- build/ia/default_ia.lua
+++ build/ia/default_ia.lua
@@ -1,12 +1,15 @@
 
 base_path = './build/ia/'
 package.path = base_path .. '?.lua;' .. package.path
+package.path = base_path .. 'enemies/?.lua;' .. package.path
+package.path = base_path .. 'basic/?.lua;' .. package.path
+package.path = base_path .. 'helpers/?.lua;' .. package.path
 
 active_debug = false
 active_debug_list = false
 
 -- require "ia_state"
--- require "ia_intro"
+require "ia_intro"
 -- require "ia_simple"
 -- require "ia_coward"
-require "ia_stalker"
+-- require "ia_stalker"
