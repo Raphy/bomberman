01ed091f2278e5624453daae7576884f8f7216ee
Nicolas Svirchevsky <nicolas.svirchevsky@epitech.eu>
2014-06-13 17:35:06 +0000

    - implemented drawGeometry && drawTexture for further optimisations

--- src/object/AGeometry.cpp
+++ src/object/AGeometry.cpp
@@ -7,6 +7,14 @@
 
 #include "AGeometry.hh"
 
+void AGeometry::drawGeometry(gdl::AShader& shader, const gdl::Clock& clock) {
+    this->_geometry.draw(shader, this->getTransformation(), GL_QUADS);
+}
+
+void AGeometry::drawTexture() {
+    this->_texture.bind();
+}
+
 void AGeometry::draw(gdl::AShader & shader, gdl::Clock const &clock)
 {
     (void) clock;
