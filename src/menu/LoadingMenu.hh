c98a3bd6aa0b1ff24775d9585d163123ed8bfc08
Damien sauvalle <damien.sauvalle@epitech.eu>
2014-06-14 07:23:06 +0200

    Loading Scene Fuck Yead

---
+++ src/menu/LoadingMenu.hh
@@ -0,0 +1,20 @@
+#ifndef LOADINGMENU_HH_
+# define LOADINGMENU_HH_
+
+# include "AMenuScene.hh"
+
+class LoadingMenu : public AMenuScene
+{
+public:
+  LoadingMenu(SceneArguments const&);
+  ~LoadingMenu();
+
+  virtual bool initialize();
+  virtual bool update(gdl::Clock const& clock, gdl::Input& input);
+  virtual bool draw(gdl::AShader & shader, gdl::Clock const &clock);
+private:
+
+
+};
+
+#endif
