36406f394209e68cd67ccf689b9fc47f3c7e3bbf
Nicolas Svirchevsky <nicolas.svirchevsky@epitech.eu>
2014-06-14 20:13:51 +0000

    FUCK tuple getPosition()! ... :P

--- src/object/Bomb.cpp
+++ src/object/Bomb.cpp
@@ -14,6 +14,7 @@ static const double BOMBTIME = 2;
 
 bool Bomb::initialize() {
     this->scale(glm::vec3(0.10, 0.10, 0.10));
+    this->_position.y += 0.25;
     return true;
 }
 
@@ -33,7 +34,7 @@ void Bomb::createFire() {
     Fire* obj = new Fire();
     
     obj->setStep(1);
-    obj->setPosition(this->_position);
+    obj->setPosition(this->_position.x, this->_position.z);
     obj->setDirection(Fire::None);
     
     addObject(obj);
