3f9339cf6f463c2306b9a7a99d7d94747d3499c1
Nicolas Svirchevsky <nicolas.svirchevsky@epitech.eu>
2014-06-15 04:56:40 +0000

    resize texture.... and colision barth reset

--- src/object/AGameObject.cpp
+++ src/object/AGameObject.cpp
@@ -7,17 +7,12 @@ void AGameObject::restoreLastState(AGameObject const& obj) {
     _position = _state.position;
     Rectangle const& collider = getCollider();
         
-    if (std::floor(std::get<0>(obj.getPosition()) + 0.5) == std::floor(this->_position.x + 0.5)) {
-        double center_x = collider.getX() + collider.getW() / 2;
-        _position.x = std::floor(center_x);
-    }
+    double center_x = collider.getX() + collider.getW() / 2;
+    double center_y = collider.getY() + collider.getH() / 2;
     
+    _position.x = std::floor(center_x);
+    _position.z = std::floor(center_y);
     
-    if (std::floor(std::get<1>(obj.getPosition()) + 0.5) == std::floor(this->_position.z + 0.5)) {
-        double center_y = collider.getY() + collider.getH() / 2;
-        _position.z = std::floor(center_y);
-    }
-
 }
 
 void AGameObject::saveCurrentState() {
