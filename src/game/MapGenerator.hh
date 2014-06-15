c557f99956ffe8e068cfaaef0dac4ac1b8edd134
Pelayo Parejo Pagador <pelayo.parejopagador@epitech.eu>
2014-06-15 05:30:36 +0000

    map generator up to the norme

--- src/game/MapGenerator.hh
+++ src/game/MapGenerator.hh
@@ -20,17 +20,19 @@ class MapGenerator
         virtual ~MapGenerator();
 
         std::pair<std::list<AGameObject*>, std::list<AGameObject*>> Generate(int width, int height);
-
+        
     private:
         int                               _width;
         int                               _height;
         std::vector<std::pair<int, int>>  _frontier;
         std::vector<std::vector<char>> *  _field;
+        std::vector<std::pair<int, int>>  _extra;
         std::pair<std::list<AGameObject*>, std::list<AGameObject*>> _objects;
         
         void Dig(int y, int x);
         bool Check(int y, int x);
         void Harden(int y, int x);
+        void setExtra(int y, int x);
         std::pair<std::list<AGameObject*>, std::list<AGameObject*>> ToList();
 };
 #endif   /* MAPGENERATOR_HH */
