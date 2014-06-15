d7678dad97871b59d1b9aa1752c3964b2638d0a1
Nicolas Svirchevsky <nicolas.svirchevsky@epitech.eu>
2014-06-15 18:35:10 +0000

    bomb_time_1

--- src/object/Bomb.hh
+++ src/object/Bomb.hh
@@ -14,7 +14,7 @@
 class Bomb: public AModel {
 
 public:
-    Bomb(): AModel(ResourcesPath::asset("bomb.fbx"), "bomb"), _time(0) { setPosition(glm::vec3(0, 0, 0)); setSpeed(0); scale(glm::vec3(0.02, 0.02, 0.02)); }
+    Bomb(): AModel(ResourcesPath::asset("bomb.fbx"), "bomb"), _time(1) { setPosition(glm::vec3(0, 0, 0)); setSpeed(0); scale(glm::vec3(0.02, 0.02, 0.02)); }
     virtual ~Bomb() {}
     virtual bool initialize();
     virtual void update(const gdl::Clock& clock, gdl::Input& input);
